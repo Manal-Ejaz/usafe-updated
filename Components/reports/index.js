b1f7e04882815c6911b1e2090317b24ea100bf00
manal-ejaz <manal.ejaz@uptodata.com>
2023-07-07 20:33:37 +0500

    Initial Commit

---
+++ Components/reports/index.js
@@ -0,0 +1,37 @@
+const reports = () => {
+    return (
+      `<div class='ReportContainer'>
+      <div class='ReportContent'>
+
+        <div class='ReportImage'>
+          <img src="../../assets/usafeLogo.png"/>
+        </div>
+
+        
+        <div class='ReportHeadings'>
+          <p>Status:</p>
+          <p>Type:</p>
+          <p>Assigned to:</p>
+          <p>Due Date:</p>
+        </div>
+
+        <div class='ReportDescriptions'>
+          <p>Pending</p>
+          <p>Hazard Reporting</p>
+          <p>Prod Area Manager</p>
+          <p>2018-05-25</p>
+        </div>
+
+      </div>
+      <div class='ReportBorder'/>
+      <button class='ReportButton'}>VIEW DETAILS</button>
+
+    </div>
+  `
+    )
+  }
+  const reportsCaller=()=>{
+      $(".reports").html(reports())
+  }
+  
+  reportsCaller();
